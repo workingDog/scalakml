13c7b2ca94e63d3d21f3816bb716cffa2f18d6dc
workingDog <kodefarmer@gmail.com>
2013-01-28 19:57:23 +1100

    test commit

--- src/com/scalakml/example/ReadWriteExample1.scala
+++ src/com/scalakml/example/ReadWriteExample1.scala
@@ -4,7 +4,7 @@ import com.scalakml.io.{KmlPrintWriter, KmlFileReader}
 import xml.PrettyPrinter
 
 /**
- * @author: Ringo Wathelet
+ * author: Ringo Wathelet
  * Date: 23/01/13 
  * Version: 1
  */
@@ -14,7 +14,7 @@ object ReadWriteExample1 {
     println("....ReadWriteExample1 start...\n")
     // read a kml file into a kml root object
     val kml = new KmlFileReader().getKmlFromFile("../kml-files/KML_Samples.kml")
-    // write the kml to an output file
+    // write the kml to an output file.
 //    if (kml.isDefined) new KmlPrintWriter("../kml-files/scalakml_KML_Samples.kml").write(kml, new PrettyPrinter(80, 3))
     // write the kml to System.out
     if (kml.isDefined) new KmlPrintWriter().write(kml, new PrettyPrinter(80, 3))
