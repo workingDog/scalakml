92ecbea950c5499c5ad974470dc342d314f6be20
workingDog <kodefarmer@gmail.com>
2013-11-06 12:36:38 +0900

    minor change to ReadWriteExample1

--- src/com/scalakml/example/ReadWriteExample1.scala
+++ src/com/scalakml/example/ReadWriteExample1.scala
@@ -13,7 +13,7 @@ object ReadWriteExample1 {
   def main(args: Array[String]) {
     println("....ReadWriteExample1 start...\n")
     // read a kml file into a kml root object
-    val kml = new KmlFileReader().getKmlFromFile("./kml-files/KML_Samples.kml")
+    val kml = new KmlFileReader().getKmlFromFile("./kml-files/example-no-cdata.kml")
 
     // write the kml to an output file.
 //    if (kml.isDefined) new KmlPrintWriter("./kml-files/scalakml_KML_Samples.kml").write(kml, new PrettyPrinter(80, 3))
